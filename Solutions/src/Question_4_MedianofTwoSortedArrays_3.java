ba476a59bde8199353cd441fd78d712df3f9d90b
Ting <wisezt@hotmail.com>
2019-08-13 22:07:35 -0500

    4. Median of Two Sorted Arrays succeed!!!

---
+++ Solutions/src/Question_4_MedianofTwoSortedArrays_3.java
@@ -0,0 +1,230 @@
+public class Question_4_MedianofTwoSortedArrays_3 {
+
+    public static void main(String[] strings) {
+
+        int[] nums1 = {};
+        int[] nums2 = {2,3};
+
+
+        System.out.println("num: " + findMedianSortedArrays(nums1, nums2));
+
+    }
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        int theTotalLength = nums1.length + nums2.length;
+        int index1=0, index2=0;
+        int nums1L = nums1.length, nums2L = nums2.length;
+        double medianIndex = Math.floor((theTotalLength - 1) / 2);
+        boolean isMedian = false;
+        int count = 0;
+        boolean isArr1 = false, isArr2 = false;
+        boolean isIndex1OutOfBorder = false;
+        boolean isIndex2OutOfBorder = false;
+
+
+        int currentIndex = 0;
+
+        int medianIndexPlus = 0, medianIndexMinus=0;
+        double medianIndexPlusValue = 0;
+        double medianIndexMinusValue = 0;
+
+        // Odd
+        if (theTotalLength % 2 == 1) {
+            medianIndex = (theTotalLength - 1) / 2;
+
+
+            for (count = 0; count <= medianIndex; count++) {
+                if (index1 >= nums1L) {
+                    isIndex1OutOfBorder = true;
+
+                    break;
+                }
+
+                if (index2 >= nums2L) {
+                    isIndex2OutOfBorder = true;
+
+                    break;
+                }
+
+                if (nums1[index1] <= nums2[index2]) {
+                    currentIndex = index1;
+                    isArr1 = true;
+                    index1++;
+                } else {
+                    currentIndex = index2;
+                    isArr1 = false;
+                    index2++;
+                }
+
+
+            }
+
+
+            if (isIndex1OutOfBorder) {
+                for (int i = count; i < medianIndex; i++) {
+                    index2++;
+                }
+
+                return nums2[index2];
+            }
+
+
+            if (isIndex2OutOfBorder) {
+                for (int i = count; i < medianIndex; i++) {
+                    index1++;
+                }
+
+                return nums1[index1];
+            }
+
+
+            if (isArr1) {
+                return nums1[currentIndex];
+
+            } else {
+                return nums2[currentIndex];
+            }
+
+
+        } else{
+
+
+            //Even
+
+            medianIndexMinus = (theTotalLength) / 2 - 1;
+            medianIndexPlus = theTotalLength/2;
+
+
+            for (count = 0; count <= medianIndexMinus; count++) {
+                System.out.println("*********for: " + "index1:" + index1 + " index2: " + index2 + " CurrentIndex: " + currentIndex );
+
+                if (index1 >= nums1L) {
+                    isIndex1OutOfBorder = true;
+
+                    break;
+                }
+
+                if (index2 >= nums2L) {
+                    isIndex2OutOfBorder = true;
+
+                    break;
+                }
+
+                if (nums1[index1] <= nums2[index2]) {
+                    currentIndex = index1;
+                    isArr1 = true;
+                    index1++;
+                } else {
+                    currentIndex = index2;
+                    isArr1 = false;
+                    index2++;
+                }
+
+
+            }
+
+
+            if (isIndex1OutOfBorder) {
+                for (int i = count; i < medianIndexMinus; i++) {
+                    index2++;
+                }
+                System.out.println("*********for: " + "index1:" + index1 + " index2: " + index2 + " CurrentIndex: " + currentIndex );
+                medianIndexPlus = index2 + 1;
+                System.out.println("medianIndexPuls: " + medianIndexPlus);
+                System.out.println("nums2[index2]" + nums2[index2]);
+                System.out.println("nums2[medianIndexPlus]: " +nums2[medianIndexPlus]);
+                System.out.println("Before return:" + ((nums2[index2] + nums2[medianIndexPlus])/2.0));
+
+
+                return ((nums2[index2] + nums2[medianIndexPlus])/2.0);
+            }
+
+
+            if (isIndex2OutOfBorder) {
+                for (int i = count; i < medianIndexMinus; i++) {
+                    index1++;
+                }
+
+                return (nums1[index1] + nums1[++index1])/2.0;
+            }
+
+
+            if (isArr1) {
+
+                medianIndexMinusValue = nums1[currentIndex];
+
+
+
+                if (index1 >= nums1L) {
+                    isIndex1OutOfBorder = true;
+                    medianIndexPlusValue = nums2[index2];
+                } else
+
+                if (index2 >= nums2L) {
+                    isIndex2OutOfBorder = true;
+                    medianIndexPlusValue = nums1[index1];
+                } else
+
+                if (nums1[index1] <= nums2[index2]) {
+                    medianIndexPlusValue = nums1[index1];
+                } else {
+                    medianIndexPlusValue = nums2[index2];
+                }
+
+
+                return (medianIndexMinusValue + medianIndexPlusValue)/2.0;
+
+            } else {
+
+                medianIndexMinusValue = nums2[currentIndex];
+
+
+
+                if (index1 >= nums1L) {
+                    isIndex1OutOfBorder = true;
+                    medianIndexPlusValue = nums2[index2];
+                } else
+
+                if (index2 >= nums2L) {
+                    isIndex2OutOfBorder = true;
+                    medianIndexPlusValue = nums1[index1];
+                } else
+
+                if (nums1[index1] <= nums2[index2]) {
+                    medianIndexPlusValue = nums1[index1];
+                } else {
+                    medianIndexPlusValue = nums2[index2];
+                }
+
+
+
+
+
+                return (medianIndexMinusValue + medianIndexPlusValue)/2.0;
+            }
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+//        return result;
+    }
+
+}
